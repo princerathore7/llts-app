54787e116c276da4deba4b6ab666bff17f0d85a9
prince rathore <your-prince242.com@gmail.com>
2025-06-22 19:11:43 +0530

    Moved app.py to backend/

--- backend/app.py
+++ backend/app.py
@@ -30,8 +30,7 @@ ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://127.0.0.1:8000",
-    "http://localhost:8000",
-    "https://llts-frontend.onrender.com"  # ✅ Corrected with comma
+    "http://localhost:8000"
 ]
 
 # ✅ CORS Setup
