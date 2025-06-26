d73adb22f7d10bf5b5d9c32e2fece6fb6951f7d8
prince rathore <your-prince242.com@gmail.com>
2025-06-26 21:11:05 +0530

    Fix CORS allowed origins list

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ ALLOWED_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://llts-app.onrender.com"
+    "https://llts-app.onrender.com",
+    "https://spiffy-eclair-f0f49f.netlify.app"
 ]
 
 # ✅ CORS Setup (r prefix is very important!)
