efa454dbb7e6539685df511917024c1405be8d80
prince rathore <your-prince242.com@gmail.com>
2025-06-30 17:33:06 +0530

    ✅ Added config.py for ALLOWED_ORIGINS import

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+import os
+
+ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://llts-app.onrender.com",
+    "https://spiffy-eclair-f0f49f.netlify.app"
+]
